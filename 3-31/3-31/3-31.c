c23a8204f20cb6885211a6a068168afe0df5236f
littleoro <101625136+littleoro@users.noreply.github.com>
2022-03-31 20:38:00 +0800

    Add files via upload

--- 3-31/3-31/3-31.c
+++ 3-31/3-31/3-31.c
@@ -45,6 +45,110 @@
 //}
 //
 
+//int my_strlen(char* arr) {
+//	int count = 0;
+//	while (*arr != '\0') {
+//		count++;
+//		arr++;
+//	}
+//	return count;
+//}
+//void change_arr(char* arr) {
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	char a = arr[left];
+//	while (left <= right) {
+//		a = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = a;
+//		left++;
+//		right--;
+//	}
+//
+//}
+//
+//int main() {
+//	char arr[] = "abcdefg";
+//	change_arr(arr);
+//	printf("%s", arr);
+//
+//
+//	return 0;
+//
+//}
 
+//int my_strlen(char *arr) {
+//	int count = 0;
+//	while (*arr != '\0') {
+//		count++;
+//		arr++;
+//	}
+//	return count;
+//}
+//
+//void change_arr(char* arr) {
+//	char tmp = *arr;
+//	int len = my_strlen(arr) - 1;
+//	if (len > 0) {
+//		*arr = *(arr + len);
+//		*(arr + len) = '\0';
+//
+//		change_arr(arr + 1);
+//	}
+//	*(arr + len) = tmp;
+//
+//}
+//
+//int main() {
+//	char arr[] = "adc";
+//	change_arr(arr);
+//	printf("%s", arr);
+//
+//
+//	return 0;
+//
+//}
+//
+//
+//int calcuate(int x) {
+//	if (x != 0) {
+//		int y = 0;
+//		y = x % 10;
+//		return y + calcuate(x / 10);
+//	}
+//	else
+//		return 0;
+//}
+//
+//
+//int main(){
+//	int a = 0;
+//	printf("输入你想计算的数字>");
+//	scanf("%d", &a);
+//	int b=calcuate(a);
+//	printf("%d", b);
+//
+//	return 0;
+//
+//}
 
-
+//int multiple(int n,int k) {
+//	if (k-1 >0) {
+//		return n * multiple(n, k - 1);
+//	}
+//	else
+//		return n;
+//}
+//
+//
+//int main() {
+//	int n = 0;
+//	int k = 0;
+//	printf("请输入n>");
+//	scanf("%d", &n);
+//	printf("请输入k>");
+//	scanf("%d", &k);
+//	int a = multiple(n, k);
+//	printf("n的k次方>%d", a);
+//	return 0;
+//}
