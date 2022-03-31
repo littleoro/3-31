ffd95bf0de924e5e14fab39aa0f8fac134a5016e
littleoro <101625136+littleoro@users.noreply.github.com>
2022-03-31 14:53:57 +0800

    Add files via upload

---
+++ 3-31/3-31/3-31.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int max(int* arr,int size) {
+//	int i = 0;
+//	int max = 0;
+//	max = *arr;
+//	for (i = 1; i < size; i++) {
+//		if (max >= *(arr + i)) {
+//			max = max;
+//		}
+//		else
+//			max = *(arr + i);
+//	}
+//	return max;
+//}
+//
+//int main() {
+//	int arr1[] = { 15,4,89,6,5,8,7,4,3,5,2,8,9 };
+//	int Max = 0;
+//	int a = 0;
+//	a = sizeof(arr1) / sizeof(arr1[0]);
+//	Max = max(arr1,a);
+//	printf("%d\n", Max);
+//	return 0;
+//}
+ 
+
+//打印九九乘法表
+//void print_table(int n) {
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= n; i++) {
+//		printf("\n");
+//		for (j = 1; j <= i; j++) {
+//			printf("%d * %d = %d ", i, j, (i * j));
+//		}
+//	}
+//}
+//int main() {
+//	int a = 0;
+//	printf("请输入想要打印的乘法口诀表的阶数>");
+//	scanf("%d", &a);
+//	print_table(a);
+//	return 0;
+//}
+//
+
+
+
+
